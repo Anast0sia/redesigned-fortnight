3d31f6e322914d162f2d8ce6e44762ddc920f5dd
Anast0sia <anastypol33@gmail.com>
2024-10-28 18:54:16 +0300

    Add packages

--- main/java/com/example/demo/repository/Repository.java
+++ main/java/com/example/demo/repository/Repository.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.repository;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
